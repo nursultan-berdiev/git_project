0196a2a5a9da2f8647d2201be13b91afa3f85e22
Nursultan Berdiev <dominguez120205024@gmail.com>
2023-01-23 10:25:34 +0600

    added permission and authentication

--- app/items/views.py
+++ app/items/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from .models import Item
 from rest_framework import viewsets
 from serializers import ItemSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
